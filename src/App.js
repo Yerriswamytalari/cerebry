d7069b4018ad3262781b6c649ded13b6a8ea98f7
iamsherlockholmes1999 <yerriswamytalari155@gmail.com>
2023-10-12 12:58:46 +0530

    cerebrytask

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      largeSquareDimension: 9,
+      smallSquareDimension: 3,
+      numCols: 0,
+      numRows: 0,
+      selectedSquare: null,
+    };
+  }
+
+  componentDidMount() {
+    const { largeSquareDimension, smallSquareDimension } = this.state;
+    const numCols = largeSquareDimension / smallSquareDimension;
+    const numRows = largeSquareDimension / smallSquareDimension;
+
+    this.setState({ numCols, numRows });
+  }
+
+  handleSquareClick = (row, col) => {
+    this.setState({ selectedSquare: { row, col } });
+  };
+
+  render() {
+    const { numCols, numRows, selectedSquare } = this.state;
+    const smallSquares = [];
+
+    for (let row = 0; row < numRows; row++) {
+      for (let col = 0; col < numCols; col++) {
+        const key = `${row}-${col}`;
+        const selected = selectedSquare && row === selectedSquare.row && col === selectedSquare.col;
+
+        smallSquares.push(
+          <div
+            key={key}
+            className={`small-square ${selected ? "selected" : ""}`}
+            onClick={() => this.handleSquareClick(row, col)}
+          >
+            {selected && `(${row}, ${col})`}
+          </div>
+        );
+      }
+    }
+
+    return (
+      <div className="App">
+        <div className="large-square">
+          {smallSquares}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
